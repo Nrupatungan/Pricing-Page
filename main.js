73b5dee9be0187b6b4591fba1358889336be6003
Dinesh Sake <dineshsake12345@gmail.com>
2022-06-04 09:19:53 +0530

    Pushing to repository

--- main.js
+++ main.js
@@ -3,10 +3,26 @@ const categories = document.querySelector('select[name="categories"]')
 const animate = document.querySelector('.animate');
 const btnContainer = document.querySelector('.btn-container');
 const table = document.querySelector('.table-container');
-const dropdownItem = document.querySelector('.currency .dropdown-menu');
+const currencies = document.querySelector('select[name="currencies"]');
+const nodelist = document.querySelectorAll('tbody tr td.pricing-cell');
+
+const prices = Array.from(nodelist);
+const priceArray = new Array();
+const noSignPrice = new Array();
+
+for(let i = 0; i < prices.length; i++){
+    const price = prices[i].textContent;
+    priceArray.push(price);
+}
+
+for(let i = 0; i < priceArray.length; i++){
+    const price = priceArray[i].replace("$", "");
+    noSignPrice.push(price);
+}
 
 products.value = "";
 categories.value = "";
+currencies.value = "inr";
 
 products.addEventListener('change', () => {
     if (products.value !== ""){
@@ -20,6 +36,7 @@ products.addEventListener('change', () => {
             } else {
                 btnContainer.classList.remove("d-block");
                 table.classList.remove("d-block");
+                currencies.value = "inr";
             }
         })
     } else {
@@ -27,23 +44,66 @@ products.addEventListener('change', () => {
         btnContainer.classList.remove("d-block");
         table.classList.remove("d-block");
         categories.value = "";
+        currencies.value = "inr";
     }
 });
 
-dropdownItem.addEventListener('click', (e) => {
-    // console.log(e.target.getAttribute('data-name'));
-    switch (true) {
-        case e.target.getAttribute('data-name') === "inr":
-            console.log('le inr');
-            break;
-    
-        case e.target.getAttribute('data-name') === "euro":
-            console.log('le euro');
-            break;
-        
-        default:
-            console.log('le usd');
-            break;
-    }
+
+// function currencyChange(e){
+//     const newPrices = new Array();
+//     const api = "https://api.exchangerate-api.com/v4/latest/USD";
+//     fetch(`${api}`)
+//     .then(currency => {
+//         return currency.json();
+//     }).then(currency => {
+//         let fromRate = currency.rates[resultFrom];
+//         let toRate = currency.rates[resultTo];
+//         console.log(fromRate);
+//         console.log(toRate);
+//     });
+// }
+
+const currencyElements = document.querySelectorAll('.currency');
+const usdChangeRate = {
+      INR: 77.39, // 1INR = 77.39 USD
+      EUR: 0.933, // 1EUR = 0.933 USD
+      USD: 1.0
+};
+
+let toCurrency;
+let el;
+let fromCurrency;
+
+currencies.addEventListener('change', () => {
+        toCurrency = currencies.value.toUpperCase();
+        for (let i=0,l=currencyElements.length; i<l; ++i) {
+            el = currencyElements[i];
+            fromCurrency = el.getAttribute("data-currencyName").toUpperCase();
+        }
+        usd(fromCurrency, toCurrency, el);
 })
+
+function usd(a, b, c){
+    for(let j = 0; j < noSignPrice.length; j++){
+        const price = noSignPrice[j];
+        console.log(price)
+        if(a in usdChangeRate){
+            let fromCurrentToUsd = price * usdChangeRate[a];
+            console.log(fromCurrentToUsd)
+            let toCurrentMoney = fromCurrentToUsd / usdChangeRate[b];
+            console.log(toCurrentMoney)
+            if (toCurrency === "INR"){
+                c.innerHTML = "<span>" + "&#8377;" + "</span>" + toCurrentMoney;
+                c.setAttribute("data-currencyName",b);
+            } else if(toCurrency === "USD"){
+                c.innerHTML = "<span>" + "&dollar;" + "</span>" + toCurrentMoney;
+                c.setAttribute("data-currencyName",b);
+            } else {
+                c.innerHTML = "<span>" + "&euro;" + "</span>" + toCurrentMoney;
+                c.setAttribute("data-currencyName",b);
+            }
+        }
+    }
+};
+
 
